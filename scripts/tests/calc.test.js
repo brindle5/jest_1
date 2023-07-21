2321120eb4d5598a04d902be307ab7196d8db317
brindle5 <sarah.brindle@hotmail.co.uk>
2023-07-21 14:17:55 +0000

    Update changes

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,25 +1,25 @@
 
 const addition = require ('../calc');
 
-describe ('Calculator', () => {
-    describe('Addition function', () => {
-        test('Should return 42 for 20 + 22', () => {
-            expect(addition(20,22)).toBe(42);
-        })
-        test('Should return 73 for 42 + 31', () => {
-            expect(addition(42,31)).toBe(73);
-        })
-    });
+// describe ('Calculator', () => {
+//     describe('Addition function', () => {
+//         test('Should return 42 for 20 + 22', () => {
+//             expect(addition(20,22)).toBe(42);
+//         })
+//         test('Should return 73 for 42 + 31', () => {
+//             expect(addition(42,31)).toBe(73);
+//         })
+//     });
 
-    describe('Subtraction function', () => {
+//     describe('Subtraction function', () => {
 
-    });
+//     });
 
-    describe('Multiplication function', () => {
+//     describe('Multiplication function', () => {
 
-    });
+//     });
 
-    describe('Divitsion function', () => {
+//     describe('Divitsion function', () => {
 
-    });
-})
+//     });
+// })
